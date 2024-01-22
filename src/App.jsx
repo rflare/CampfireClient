cd579ad5b06a2fa9df7ecb5068f7ae101639abbe
rusluck <russellymallari@gmail.com>
2024-01-22 16:55:38 -0500

    I dunno what to say

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
 import { useState, useEffect } from 'react'
 
 function App() {
-    const [data, setData] = useState("Hi react")
+    const [username, setUsername] = useState("")
+    const [bodyText, setBodyText] = useState("")
+
+    const [posts, setPosts] = useState("")
 
     useEffect(() => {
-        fetch("/api/get")
+        fetch("http://localhost:53342/api/get")
             .then((res) => res.json())
             .then((res) => {
-                console.log(res)
-                setData(res[0].text)
+                setPosts(res[1].text)
             })
-    })
+    }, [])
+    
+
+
+    function postData(username, bodytext)
+    {
+
+        fetch("http://localhost:53342/api/post",{
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: username,
+                text: bodytext,
+                time: Date.now()
+            }),
+        })
+
+    }
+
     return (
-        <h1>{data}</h1>
+        <>
+            <h1>{2 + 2}{posts}</h1>
+
+
+            <input type="text" value={username} maxLength="30" onChange={(e) => setUsername(e.target.value)}/>
+            <input type="text" value={bodyText} onChange={(e) => setBodyText(e.target.value)}/>
+
+
+            <p>Name: {username}</p>
+            <p>Text: {bodyText}</p>
+
+            <button onClick={()=>postData(username, bodyText)}>Send post</button>
+        </>
     )
 }
 
