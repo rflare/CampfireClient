34180c85a1383f2434bf977e72ee445d76603f76
rusluck <russellymallari@gmail.com>
2024-01-31 08:40:27 +0000

    Done refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,14 @@ import { TopSection } from './TopSection.jsx'
 import { MiddleSection } from './MiddleSection.jsx'
 import { BottomSection } from './BottomSection.jsx'
 
-<<<<<<< HEAD
-// Main app
-
 export default function App() {
-    //Main State for the application
-    //Includes:
-    // - Username for post
-    // - Body text for post
-    // - Feed containing posts that client recieves per reload
-    // Note: Can only fetch 100 posts per reload
-    // - Error state as boolean
-    //    - If there is error, app assumes as 500 interal server error
-    //
-=======
-export default function App() {
->>>>>>> 8043af141fd56dffed23557404621b576a3f7087
     const [username, setUsername] = useState("")
     const [bodyText, setBodyText] = useState("")
 
     const [feed, setFeed] = useState([])
     const [err, setErr] = useState(false)
 
-<<<<<<< HEAD
     //Fetches posts per reload
-=======
->>>>>>> 8043af141fd56dffed23557404621b576a3f7087
     useEffect(() => {
         downloadPosts(setErr, setFeed)
     }, [])
@@ -39,22 +21,15 @@ export default function App() {
 
     if(err)
     {
-<<<<<<< HEAD
         //Error page if could not fetch posts
-=======
->>>>>>> 8043af141fd56dffed23557404621b576a3f7087
         return <ErrorSection />
     }
     return (
         <>
-<<<<<<< HEAD
             {
                  //Main top section
                 //includes main title and stuff for uploading   
             }
-=======
->>>>>>> 8043af141fd56dffed23557404621b576a3f7087
-
             <TopSection
                 username={username} setUsername={setUsername}
                 bodyText={bodyText} setBodyText={setBodyText}
@@ -62,25 +37,19 @@ export default function App() {
 
             <br/>
 
-<<<<<<< HEAD
             {
 
                 //Middle section
                 //Contains feed with posts
             }
-=======
->>>>>>> 8043af141fd56dffed23557404621b576a3f7087
             <MiddleSection content={feed} />
 
             <br/>
 
-<<<<<<< HEAD
             {
                 //Bottom Section
                 //Contains useless information that is critical knowledge to the average viewer
             }
-=======
->>>>>>> 8043af141fd56dffed23557404621b576a3f7087
             <BottomSection />
 
 
