37581d7617e1cd00ccac032b113123879958231e
rusluck <russellymallari@gmail.com>
2024-02-06 11:22:22 +0000

    Added a new page. App is now in /app

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig((command, mode) => {
     const env = loadEnv(mode, process.cwd(), '')
     return {
+        build: {
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, 'index.html'),
+                    app: resolve(__dirname, 'app/index.html')
+                }
+            }
+        },
         plugins: [react()],
         server:{
             host: '127.0.0.1',
