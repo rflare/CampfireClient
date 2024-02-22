15c222bc62b0020ce7f416d2cfe2ae45f029ae09
rflare <russellymallari@gmail.com>
2024-02-22 11:36:00 -0500

    Cooler button

--- app/page.js
+++ app/page.js
@@ -4,6 +4,16 @@ export default function Home() {
     return <div className="homePage">
         <h1>Campfire</h1>
         <h2>An open source place</h2>
-        <a href="/app">Get Started now!</a>
+        <a
+            href="/app"
+            className="clickMeButton"
+            style={{
+                width: "200px",
+                height: "70px",
+                fontSize: "20px"
+            }}
+        >
+            Get Started now!
+        </a>
     </div>
 }
